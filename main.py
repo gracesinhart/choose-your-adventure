79c2d79597c37506cbc2aa235634ea155b78dd4e
Isaac Zeller <95357623+IsaacZel@users.noreply.github.com>
2021-12-09 19:00:25 +0000

    uhhh some more code i guess lol

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ print("Welcome player")
 choice1 = input("Do you wish to start the experience? [Yes] or [No]?")
 
 if choice1 == 'No':
+  print("Ending 1: nothing")
   print("Alright.")
   print("Try again.")
 else:
   print("Okay.")
 
-  
+choice1b = input("Do you walk your dog? [Yes] or [No]")
+
+if choice1b == "No":
+  print("Your dog cries and paws at you incessantly")
+  print("Try gain?")
