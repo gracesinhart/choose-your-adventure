d690899c4e5d74301cf3ef0ec99d2dc574ba123a
Grace Sinhart <95306377+gracesinhart@users.noreply.github.com>
2021-12-09 18:46:47 +0000

    yes is an option now

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ print("Welcome player")
 choice1 = input("Do you wish to start the experience? [Yes] or [No]?")
 
 if choice1 == 'No':
-  print("/kill player")
+  print("Alright.")
   print("Try again.")
+else:
+  print("Okay.")
+
+  
